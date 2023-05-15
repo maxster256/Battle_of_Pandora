a66df35450a2c4201695be1f794979408a31bb7f
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-05-15 20:24:11 +0200

    Update Robot.java

--- Project/lib/src/main/java/pandora/Robot.java
+++ Project/lib/src/main/java/pandora/Robot.java
@@ -12,4 +12,8 @@ public class Robot extends Unit_Strength implements Interface{
 	{
 		
 	}
+	public Robot(int health, int speed, int pos_x, int pos_y,int strength, int index)
+	{
+		super(health,speed,pos_x,pos_y,strength,index);
+	}
 }
