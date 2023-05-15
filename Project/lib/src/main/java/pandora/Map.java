32987902b7b525962733d0a2535ec7ffaa72e2b1
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-05-15 20:23:31 +0200

    Update Map.java

--- Project/lib/src/main/java/pandora/Map.java
+++ Project/lib/src/main/java/pandora/Map.java
@@ -1,5 +1,68 @@
 package pandora;
 
-public class Map {
+import java.util.Random;
 
+public class Map {
+	
+	private int width;
+    private int height;
+    private int density; // zagęszczenie roślin w procentach
+    private int Trees_numb;
+    private int Bushes_numb;
+    private char[][] map;
+	
+    public Map()
+    {
+    	
+    }
+	public Map(int width, int height, int plantDensity) {
+        this.width = width;
+        this.height = height;
+        this.density = plantDensity;
+        this.map = new char[height][width];
+        this.Trees_numb = 0;
+        this.Bushes_numb = 0;
+    }
+	// metoda generująca mapę
+    public void generate() {
+        Random rand = new Random();
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                // losowe umieszczenie roślin na mapie
+                if (rand.nextInt(100) < density) {
+                    // losowy wybór typu rośliny
+                    if (rand.nextInt(4) == 0) {
+                        map[row][col] = 'T'; // T-drzewo
+                        Trees_numb++;
+                    } else {
+                        map[row][col] = 'B'; // B-krzak
+                        Bushes_numb++;
+                    }
+                } else {
+                    map[row][col] = '_'; // pole puste
+                }
+            }
+        }
+    }
+
+    // metoda wyświetlająca mapę na ekranie
+    public void display() {
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                System.out.print(map[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // metoda zwracająca liczbę T-drzew na mapie
+    public int getTrees_numb() {
+        return Trees_numb;
+    }
+
+    // metoda zwracająca liczbę B-krzaków na mapie
+    public int getBushes_numb() {
+        return Bushes_numb;
+    }
 }
