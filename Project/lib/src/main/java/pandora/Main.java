20fc6055af69c1d0e94129a0fd1d4f493b11d2ab
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-05-15 20:23:02 +0200

    Update Main.java

--- Project/lib/src/main/java/pandora/Main.java
+++ Project/lib/src/main/java/pandora/Main.java
@@ -4,14 +4,41 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args)
+	public static void main(String[] args)	//metoda na parametry wejsciowe
 	{
 		int x,y;
-		System.out.println("Podaj rozmiar mapy:");
+		int rider, archer, robot, soldier;
+		int density;
+		int iterations;
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Podaj rozmiar mapy:");
 		x = scan.nextInt();
 		y = scan.nextInt();
+		System.out.println("Podaj liczebnosc jezdzcow:");
+		rider=scan.nextInt();
+		System.out.println("Podaj liczebnosc lucznikow:");
+		archer=scan.nextInt();
+		System.out.println("Podaj liczebnosc robotow:");
+		robot=scan.nextInt();
+		System.out.println("Podaj liczebnosc zolnierzy:");
+		soldier=scan.nextInt();
+		System.out.println("Podaj zageszczenie lasu:");
+		density=scan.nextInt();
+		System.out.println("Podaj liczbe iteracji:");
+		iterations=scan.nextInt();
+		//...
+		Main.simulation(x,y,rider,archer,robot,soldier,iterations,density);
 		scan.close();
 	}
-}
+	
+	public static void simulation(int x, int y, int rider, int archer, int robot, int soldier, int iterations, int density) //metoda na wypadek skryptow z podanymi parametrami
+	{
+		Map mapa = new Map(x,y,density);
+		mapa.generate();		//stworzenie mapy
+		for(int i=0;i<iterations;i++)
+		{
+			//wykonanie symulacji	
+		}
+	}
 
+}
