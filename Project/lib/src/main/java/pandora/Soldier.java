19db7c19bb442807825cd3812ce2f2457d672d7e
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-05-15 20:24:38 +0200

    Update Soldier.java

--- Project/lib/src/main/java/pandora/Soldier.java
+++ Project/lib/src/main/java/pandora/Soldier.java
@@ -12,4 +12,8 @@ public class Soldier extends Unit_Strength implements Interface{
 	{
 		
 	}
+	public Soldier(int health, int speed, int pos_x, int pos_y,int strength, int index)
+	{
+		super(health,speed,pos_x,pos_y,strength,index);
+	}
 }
