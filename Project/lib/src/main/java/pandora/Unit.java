0303cd0a89e684318e62169d6d571ae4989e9c2a
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-05-15 20:25:00 +0200

    Update Unit.java

--- Project/lib/src/main/java/pandora/Unit.java
+++ Project/lib/src/main/java/pandora/Unit.java
@@ -3,4 +3,13 @@ package pandora;
 public abstract class Unit {
 	int health;
 	int speed;
+	int pos_x;
+	int pos_y;
+	public Unit(int health, int speed, int pos_x, int pos_y)
+	{
+		this.health=health;
+		this.speed=speed;
+		this.pos_x=pos_x;
+		this.pos_y=pos_y;
+	}
 }
