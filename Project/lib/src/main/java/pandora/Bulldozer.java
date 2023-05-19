359fe8bc69a893d0a0463eb19bc1207f546c56fc
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-05-19 22:43:40 +0200

    Update Bulldozer.java

--- Project/lib/src/main/java/pandora/Bulldozer.java
+++ Project/lib/src/main/java/pandora/Bulldozer.java
@@ -1,6 +1,10 @@
 package pandora;
 
+import java.util.Random;
+
 public class Bulldozer extends Unit{
+	
+	Map mapa = new Map();
 	
 	public void move()
 	{
@@ -9,41 +13,44 @@ public class Bulldozer extends Unit{
 		
 		if (direction == 0) { // Poruszaj się w górę
         		if (pos_y - 1 >= 0) { // Sprawdź, czy nowa pozycja mieści się w granicach siatki
-           			if (isFieldEmpty(pos_x, pos_y - 1)) { // Sprawdź, czy pole jest puste
+           			if (mapa.isFieldEmpty(pos_x, pos_y - 1)) { // Sprawdź, czy pole jest puste
                				 pos_y--; // Przejdź na nową pozycję
             }
 				else {
 					destroy(); // Zniszcz pole, jeśli nie jest puste
+					pos_y--;
            	 }}}
 		else if (direction == 1) { // Poruszaj się w dół
-       			if (pos_y + 1 < GRID_SIZE) {
-            			if (isFieldEmpty(pos_x, pos_y + 1)) {
+       			if (pos_y + 1 < Main.y) {
+            			if (mapa.isFieldEmpty(pos_x, pos_y + 1)) {
               				pos_y++;
             } 
 				else {
 					destroy();
+					pos_y++;
             }}}
 		else if (direction == 2) { // Poruszaj się w lewo
        			if (pos_x - 1 >= 0) {
-          			if (isFieldEmpty(pos_x - 1, pos_y)) {
+          			if (mapa.isFieldEmpty(pos_x - 1, pos_y)) {
              				pos_x--;
             }
 				else {
 					destroy();
+					pos_x--;
             }}}
 		else if (direction == 3) { // Poruszaj się w prawo
-       			if (pos_x + 1 < GRID_SIZE) {
-           			if (isFieldEmpty(pos_x + 1, pos_y)) {
+       			if (pos_x + 1 < Main.x) {
+           			if (mapa.isFieldEmpty(pos_x + 1, pos_y)) {
               				pos_x++;
             } 
 				else {
 					destroy();
-            }}}
-		
+					pos_x++;
+            }}}	
 	}
 	public void destroy()
 	{
-		map[pos_x][pos_y] = '_'; // Zamień zawartość pola na '_'
+		mapa.change_map(pos_x,pos_y);
 	}
 	public Bulldozer(int health, int speed, int pos_x, int pos_y)
 	{
