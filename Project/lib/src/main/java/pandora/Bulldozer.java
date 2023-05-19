778702e1c619db5564fb7f4b506d7e4981cc420b
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-05-19 22:58:34 +0200

    Update Bulldozer.java

--- Project/lib/src/main/java/pandora/Bulldozer.java
+++ Project/lib/src/main/java/pandora/Bulldozer.java
@@ -11,46 +11,50 @@ public class Bulldozer extends Unit{
 		Random random = new Random();
 		int direction = random.nextInt(4); // GGeneruj losową liczbę między 0-3
 		
-		if (direction == 0) { // Poruszaj się w górę
+		// Poruszaj się w górę
+		if (direction == 0) {
         		if (pos_y - 1 >= 0) { // Sprawdź, czy nowa pozycja mieści się w granicach siatki
            			if (mapa.isFieldEmpty(pos_x, pos_y - 1)) { // Sprawdź, czy pole jest puste
                				 pos_y--; // Przejdź na nową pozycję
-            }
+           	}
 				else {
 					destroy(); // Zniszcz pole, jeśli nie jest puste
 					pos_y--;
-           	 }}}
-		else if (direction == 1) { // Poruszaj się w dół
+           	}}}
+		// Poruszaj się w dół
+		else if (direction == 1) { 
        			if (pos_y + 1 < Main.y) {
             			if (mapa.isFieldEmpty(pos_x, pos_y + 1)) {
               				pos_y++;
-            } 
+            	} 
 				else {
 					destroy();
 					pos_y++;
-            }}}
-		else if (direction == 2) { // Poruszaj się w lewo
+            	}}}
+		// Poruszaj się w lewo
+		else if (direction == 2) {
        			if (pos_x - 1 >= 0) {
           			if (mapa.isFieldEmpty(pos_x - 1, pos_y)) {
              				pos_x--;
-            }
+          	}
 				else {
 					destroy();
 					pos_x--;
-            }}}
-		else if (direction == 3) { // Poruszaj się w prawo
+        	}}}
+		// Poruszaj się w prawo
+		else if (direction == 3) {
        			if (pos_x + 1 < Main.x) {
            			if (mapa.isFieldEmpty(pos_x + 1, pos_y)) {
               				pos_x++;
-            } 
+           	} 
 				else {
 					destroy();
 					pos_x++;
-            }}}	
+            	}}}	
 	}
 	public void destroy()
 	{
-		mapa.change_map(pos_x,pos_y);	//wywolanie metody z klasy map zmieniajacej wartosc pola na ktorym stoi buldozer
+		mapa.change_map(pos_x,pos_y); // wywolanie metody z klasy Map odpowiedzialnej za zmiane zawartosci danego pola mapy
 	}
 	public Bulldozer(int health, int speed, int pos_x, int pos_y)
 	{
