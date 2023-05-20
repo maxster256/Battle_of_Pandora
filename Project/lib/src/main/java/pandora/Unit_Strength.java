1d72a1df247857a66c0efc64dfcf1b479daa1b70
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-05-20 19:26:38 +0200

    Update Unit_Strength.java

--- Project/lib/src/main/java/pandora/Unit_Strength.java
+++ Project/lib/src/main/java/pandora/Unit_Strength.java
@@ -4,7 +4,7 @@ public abstract class Unit_Strength extends Unit{
 	int strength;
 	int index;
 	
-	public Unit_Strength(int health, int speed, int pos_x, int pos_y,int strength, int index)
+	public Unit_Strength(int health, double speed, int pos_x, int pos_y,int strength, int index)
 	{
 		super(health,speed,pos_x,pos_y);
 	}
