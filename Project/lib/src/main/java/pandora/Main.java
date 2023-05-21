5f083c1535f0c3d6cb562b194082a129268d069c
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-05-21 19:56:26 +0200

    Update Main.java

--- Project/lib/src/main/java/pandora/Main.java
+++ Project/lib/src/main/java/pandora/Main.java
@@ -37,7 +37,48 @@ public class Main {
 		Map mapa = new Map(x,y,density);
 		mapa.generate();		//utworzenie mapy
 		mapa.display();
-		for(int i=0;i<iterations;i++)
+		
+		ArrayList<Object> Soldiers = new ArrayList<Object>();	//lista zolnierzy
+		ArrayList<Object> Robots = new ArrayList<Object>();	//lista robotow
+		ArrayList<Object> Bulldozers = new ArrayList<Object>();	//lista buldozerow - domyslne jest tylko jeden
+		ArrayList<Object> Archers = new ArrayList<Object>();	//lista lucznikow
+		ArrayList<Object> Riders = new ArrayList<Object>();	//lista jezdzcow
+		
+		ArrayList<ArrayList<Object>> Navi = new ArrayList<ArrayList<Object>>();		//lista strony navi - lucznikow i jezdzcow
+		ArrayList<ArrayList<Object>> Colonizators = new ArrayList<ArrayList<Object>>();	//lista strony kolonizatorow - zolnierzy, robotow i buldozera
+		
+		Bulldozer Bulldozer = new Bulldozer(500,0.25,x-1,y-1,0);	//utworzenie buldozera
+		Bulldozers.add(Bulldozer);					//dodanie buldozera do listy
+		//ponizsze pętle for tworza podana wczesniej liczbe jednostek poszczegolnych klas - podana liczbe razy tworza jednostke i dodaja ja do odpowiedniej listy
+		//parametry konstruktorow danych klas uszeregowane sa w nastepujacy sposob: (health,speed,pos_x,pos_y,index,strength)
+		for(int i=0;i<soldier;i++)	//petla for dla zolnierzy
+		{
+			Soldier Soldier = new Soldier(100,1,0,0,i+1,40);
+			Soldiers.add(Soldier);
+		}
+		for(int i=0;i<robot;i++)	//petla for dla robotow
+		{
+			Robot Robot = new Robot(200,2,0,0,i+1+soldier,60);
+			Robots.add(Robot);
+		}
+		for(int i=0;i<rider;i++)	//petla for dla jezdzcow
+		{
+			Rider Rider = new Rider(100,2,0,0,i+1,40);
+			Riders.add(Rider);
+		}
+		for(int i=0;i<archer;i++)	//petla for dla lucznikow
+		{
+			Archer Archer = new Archer(100,1,0,0,i+1+rider,40);
+			Archers.add(Archer);
+		}
+		
+		Navi.add(Riders);		//dodanie listy jezdzcow do listy navi
+		Navi.add(Archers);		//dodanie listy lucznikow do listy navi
+		Colonizators.add(Bulldozers);	//dodanie listy buldozera do listy kolonizatorow
+		Colonizators.add(Soldiers);	//dodanie listy zolnierzy do listy kolonizatorow
+		Colonizators.add(Robots);	//dodanie listy robotow do listy kolonizatorow
+		
+		for(int i=0;i<iterations;i++)	//petla for do wykonania symulacji
 		{
 			//wykonanie symulacji
 		}
