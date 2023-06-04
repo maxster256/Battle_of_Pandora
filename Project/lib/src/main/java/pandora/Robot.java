fbaf552209c9839fff52ae617c83e7545e1f9664
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-06-04 20:20:19 +0200

    Update Robot.java

--- Project/lib/src/main/java/pandora/Robot.java
+++ Project/lib/src/main/java/pandora/Robot.java
@@ -2,8 +2,8 @@ package pandora;
 
 public class Robot extends Unit{
 	
-	public Robot(char team,int health, double speed, int pos_x, int pos_y,int strength,double strength_bonus)
+	public Robot(int type,char team,int health, double speed, int pos_x, int pos_y, int strength,double strength_bonus)
 	{
-		super(team,health,speed,pos_x,pos_y,strength,strength_bonus);
+		super(type,team,health,speed,pos_x,pos_y,strength,strength_bonus);
 	}
 }
