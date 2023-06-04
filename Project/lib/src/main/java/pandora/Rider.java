837fdc11f9d3eab87c0012594223140f02151260
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-06-04 20:20:05 +0200

    Update Rider.java

--- Project/lib/src/main/java/pandora/Rider.java
+++ Project/lib/src/main/java/pandora/Rider.java
@@ -2,8 +2,8 @@ package pandora;
 
 public class Rider extends Unit{
 	
-	public Rider(char team,int health, double speed, int pos_x, int pos_y,int strength,double strength_bonus)
+	public Rider(int type,char team,int health, double speed, int pos_x, int pos_y, int strength,double strength_bonus)
 	{
-		super(team,health,speed,pos_x,pos_y,strength,strength_bonus);
+		super(type,team,health,speed,pos_x,pos_y,strength,strength_bonus);
 	}
 }
