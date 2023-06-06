f16e09a8469220a824fba65c5b300d70eb929654
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-06-07 00:51:37 +0200

    Update Soldier.java

--- Project/lib/src/main/java/pandora/Soldier.java
+++ Project/lib/src/main/java/pandora/Soldier.java
@@ -2,8 +2,8 @@ package pandora;
 
 public class Soldier extends Unit{
 	
-	public Soldier(char team,int health, double speed, int pos_x, int pos_y,int strength,double strength_bonus)
+	public Soldier(int type,int health, double speed, int pos_x, int pos_y, int strength,double strength_bonus,double defense_bonus, boolean can_far_attack)
 	{
-		super(team,health,speed,pos_x,pos_y,strength,strength_bonus);
+		super(type,health,speed,pos_x,pos_y,strength,strength_bonus,defense_bonus,can_far_attack);
 	}
 }
