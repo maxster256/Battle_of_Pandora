c5f43c672a0c709913f135f5673e0dd240c2aefc
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-06-07 00:48:41 +0200

    Update Archer.java

--- Project/lib/src/main/java/pandora/Archer.java
+++ Project/lib/src/main/java/pandora/Archer.java
@@ -2,8 +2,8 @@ package pandora;
 
 public class Archer extends Unit{
 	
-	public Archer(char team,int health, double speed, int pos_x, int pos_y,int strength,double strength_bonus)
+	public Archer(int type,int health, double speed, int pos_x, int pos_y, int strength,double strength_bonus,double defense_bonus, boolean can_far_attack)
 	{
-		super(team,health,speed,pos_x,pos_y,strength,strength_bonus);
+		super(type,health,speed,pos_x,pos_y,strength,strength_bonus,defense_bonus,can_far_attack);
 	}
 }
