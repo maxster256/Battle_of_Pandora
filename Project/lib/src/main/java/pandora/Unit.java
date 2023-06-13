78d892ba3b358cfb2484bcf1b65394f867468870
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-06-14 00:43:47 +0200

    Update Unit.java

--- Project/lib/src/main/java/pandora/Unit.java
+++ Project/lib/src/main/java/pandora/Unit.java
@@ -5,16 +5,17 @@ import java.util.Random;
 public class Unit implements Interface{
 	
 	int type;
-	char team;
 	int health;
 	double speed;
 	int pos_x;
 	int pos_y;
-	double moves;
 	int strength;
 	double strength_bonus;
 	double defense_bonus;
 	boolean can_far_attack;
+	int view_range;
+	
+	double moves;
 	
 	Random random = new Random();
 	
@@ -36,7 +37,7 @@ public class Unit implements Interface{
        			if (mapa.FieldContent(pos_x, pos_y - 1)!='T') {pos_y--;} // Sprawdz czy pole jest drzewem. Jesli nie, przejdź na nową pozycję
 			}break;
 		case 1: // Poruszaj się w dół
-			if (pos_y + 1 < Main.getY()) {
+			if (pos_y + 1 < mapa.getY()) {
     			if (mapa.FieldContent(pos_x, pos_y + 1)!='T') {pos_y++;}
 			}break;
 		case 2: // Poruszaj się w lewo
@@ -44,7 +45,7 @@ public class Unit implements Interface{
       			if (mapa.FieldContent(pos_x - 1, pos_y)!='T') {pos_x--;}
 			}break;
 		case 3: // Poruszaj się w prawo
-			if (pos_x + 1 < Main.getX()) {
+			if (pos_x + 1 < mapa.getX()) {
        			if (mapa.FieldContent(pos_x + 1, pos_y)!='T') {pos_x++;}
 			}break;
 		}
@@ -57,8 +58,12 @@ public class Unit implements Interface{
 		if(type==0) {((Unit)enemy).health=0;} //jesli atakujacy to buldozer to zabija przeciwnika
 		else if(random.nextFloat(1)>((Unit)enemy).defense_bonus)	//losowanie szansy na obrone ataku
 		{
-		if(mapa.FieldContent(pos_x,pos_y)=='_') {((Unit)enemy).health-=strength;}	//atak z pustego pola
-		else {((Unit)enemy).health-=strength*strength_bonus;}						//atak z krzakow
+			if(mapa.FieldContent(pos_x,pos_y)=='_') {((Unit)enemy).health-=strength;}	//atak z pustego pola
+			else {((Unit)enemy).health-=strength*strength_bonus;}						//atak z krzakow
+		}
+		if(((Unit)enemy).health<=0)
+		{
+			((Unit)enemy).pos_x=mapa.getX()+10; ((Unit)enemy).pos_y=mapa.getY()+10;
 		}
 	}
 	@Override
@@ -71,7 +76,7 @@ public class Unit implements Interface{
 			if(((Unit)enemy).pos_y-pos_y>0) {i=1;}
 			else 							{i=-1;}
 			if(mapa.FieldContent(pos_x,pos_y+i)=='T'||mapa.FieldContent(pos_x,pos_y+2*i)=='T') {attack_blocked=true;}
-			else if ((type==3||type==4) && pos_y+4*i>0 && pos_y+4*i<Main.getY())
+			else if ((type==3||type==4) && pos_y+4*i>0 && pos_y+4*i<mapa.getY())
 			{if(mapa.FieldContent(pos_x,pos_y+3*i)=='T'||mapa.FieldContent(pos_x,pos_y+4*i)=='T') {attack_blocked=true;}}
 		}
 		else if (((Unit)enemy).pos_y-pos_y==0)
@@ -79,7 +84,7 @@ public class Unit implements Interface{
 			if(((Unit)enemy).pos_x-pos_x>0) {i=1;}
 			else 							{i=-1;}
 			if(mapa.FieldContent(pos_x+i,pos_y)=='T'||mapa.FieldContent(pos_x+2*i,pos_y)=='T') {attack_blocked=true;}
-			else if ((type==3||type==4) && pos_x+4*i>0 && pos_x+4*i<Main.getX())
+			else if ((type==3||type==4) && pos_x+4*i>0 && pos_x+4*i<mapa.getX())
 			{if(mapa.FieldContent(pos_x+3*i,pos_y)=='T'||mapa.FieldContent(pos_x+4*i,pos_y)=='T') {attack_blocked=true;}}
 		}
 		if(attack_blocked==false)	//jesli atak nie zostal zablokowany wykonac atak
@@ -94,9 +99,31 @@ public class Unit implements Interface{
 				else {((Unit)enemy).health-=strength*strength_bonus*strength_bonus;}		//atak z krzakow
 			}
 		}
+		if(((Unit)enemy).health<=0)
+		{
+			((Unit)enemy).pos_x=mapa.getX()+10; ((Unit)enemy).pos_y=mapa.getY()+10;
+		}
    	}
+	@Override
+	public void find_enemy(Interface[] enemy, Map mapa)
+	{
+		int X, Y, Xe, Ye;
+		X=pos_x;
+		Y=pos_y;
+		for(Interface ENEMY : enemy) //petla foreach do znalezienia przeciwnika do zaatakowania
+		{
+			if(((Unit)ENEMY).health>0) {	//uniemozliwienie zaatakowania jednostki juz martwej
+			Xe=((Unit)ENEMY).pos_x;
+			Ye=((Unit)ENEMY).pos_y;
+			int distance = Math.abs(X-Xe)+Math.abs(Y-Ye);
+			if(distance<=1) {attack(ENEMY,mapa); pandora.Main.attack_flag=true;} //wykonanie ataku jesli wroga jednostka jest w bezposrednim sasiedztwie (do 1 kratki odleglosci)
+			else if(distance<=view_range && can_far_attack) {far_attack(ENEMY,mapa); pandora.Main.attack_flag=true;} //wykonanie ataku jesli wroga jednostka jest w odleglosci do 3 kratek
+			if(((Unit)ENEMY).health<=0) {pandora.Main.dead_unit_flag=true;}
+			break;} //jesli atak zostal wykonany to zakonczyc szukanie przeciwnika
+		}
+	}
 	
-	public Unit(int type,int health, double speed, int pos_x, int pos_y,int strength,double strength_bonus,double defense_bonus,boolean can_far_attack)
+	public Unit(int type,int health, double speed, int pos_x, int pos_y,int strength,double strength_bonus,double defense_bonus,boolean can_far_attack,int view_range)
 	{
 		this.type=type;	
 		this.health=health;	
@@ -107,5 +134,6 @@ public class Unit implements Interface{
 		this.strength_bonus=strength_bonus;
 		this.defense_bonus=defense_bonus;
 		this.can_far_attack=can_far_attack;
+		this.view_range=view_range;
 	}
 }
