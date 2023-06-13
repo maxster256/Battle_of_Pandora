8cbd43c2adce5af82217712a02f436ad1452d760
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-06-14 00:42:03 +0200

    Update Interface.java

--- Project/lib/src/main/java/pandora/Interface.java
+++ Project/lib/src/main/java/pandora/Interface.java
@@ -4,4 +4,5 @@ public interface Interface {
 	public void move(Map mapa);
 	public void attack(Interface enemy, Map mapa);
 	public void far_attack(Interface enemy, Map mapa);
+	public void find_enemy(Interface[] enemy, Map mapa);
 }
